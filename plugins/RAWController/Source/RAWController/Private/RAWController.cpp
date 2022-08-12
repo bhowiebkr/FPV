c4781a671f60fb2424a3f2b41c2fe2bdb536ca53
Bryan <bhowiebkr@gmail.com>
2022-08-12 02:09:06 -0400

    starting of getting axis to show up. Not working yet.

--- plugins/RAWController/Source/RAWController/Private/RAWController.cpp
+++ plugins/RAWController/Source/RAWController/Private/RAWController.cpp
@@ -2,19 +2,31 @@
 #include "IInputDeviceModule.h"
 #include "IInputDevice.h"
 
-
-
-
 #define LOCTEXT_NAMESPACE "FRAWControllerPlugin"
+
+const FGamepadKeyNames::Type FRawInputKeyNames::HIDController_Axis1("HIDController_Axis1");
+const FKey FRawInputKeys::HIDController_Axis1(FRawInputKeyNames::HIDController_Axis1);
 
 
 TSharedPtr<class IInputDevice> FRAWControllerPlugin::CreateInputDevice(const TSharedRef<FGenericApplicationMessageHandler>& InMessageHandler)
 {
 	return TSharedPtr<class IInputDevice>();
+
+
 }
 
 void FRAWControllerPlugin::StartupModule()
 {
+	const FName NAME_GenericUSBController(TEXT("HIDController"));
+
+	// Generic USB Controllers (Wheel, Flightstick etc.)
+	EKeys::AddMenuCategoryDisplayInfo(NAME_GenericUSBController, LOCTEXT("HIDControllerSubCateogry", "HIDController"), TEXT("GraphEditor.KeyEvent_16x"));
+
+
+	EKeys::AddKey(FKeyDetails(FRawInputKeys::HIDController_Axis1, LOCTEXT("GenericUSBController_Axis1", "GenericUSBController Axis 1"), FKeyDetails::GamepadKey, NAME_GenericUSBController));
+
+
+
 	// register our handler
 	FWindowsApplication* Application = GetApplication();
 
